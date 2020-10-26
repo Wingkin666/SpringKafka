1c458465077a0f756986c7e0662d3372d07e799b
Wingkin <1227012915@qq.com>
2020-10-26 15:48:53 +0800

    SpringBoot整合kafka 增加日志

--- src/main/java/com/example/springkafka/consumer/ConsumerTest.java
+++ src/main/java/com/example/springkafka/consumer/ConsumerTest.java
@@ -3,6 +3,7 @@ package com.example.springkafka.consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
@@ -12,6 +13,9 @@ public class ConsumerTest {
         // 消费的哪个topic、partition的消息,打印出消息内容
         //System.out.println("简单消费："+record.offset()+"-"+record.partition()+"-"+record.value());
         log.info("简单消费："+record.offset()+"-"+record.partition()+"-"+record.value());
+        //JSONObject json1 = JSONObject.parseObject((String) record.value());
+        //log.info(json1.getString("yzd"));
+        //log.info(json1.getString("fhh"));
     }
 
 }
